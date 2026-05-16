d12196ef4db0d026e061a6aa4e0f86cd4e483fdd
M Fadlika <mfadlika@outlook.com>
2026-05-16 14:34:57 +0700

    fix gateway controller type imports and install axios

--- Api/src/gateway/gateway.controller.ts
+++ Api/src/gateway/gateway.controller.ts
@@ -4,7 +4,7 @@ import {
   Req,
   Res,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import { GatewayService } from './gateway.service';
 
 @Controller()
