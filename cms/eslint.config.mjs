8a08030033035481190e9815d139a4eb06224f86
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-20 23:18:46 +0700

    cms: tambah bagian 2 eslint.config.mjs

--- cms/eslint.config.mjs
+++ cms/eslint.config.mjs
@@ -3,3 +3,8 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
