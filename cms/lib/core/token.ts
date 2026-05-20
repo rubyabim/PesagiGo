fcee61114616084a20dc7574a26579ca473f8ae0
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-20 23:26:12 +0700

    cms: tambah bagian 1 token.ts

---
+++ cms/lib/core/token.ts
@@ -0,0 +1,9 @@
+﻿import { jwtDecode } from 'jwt-decode';
+
+type JwtPayload = {
+  exp?: number;
+};
+
+export function isTokenExpired(token: string) {
+  try {
+    const decoded = jwtDecode<JwtPayload>(token);
