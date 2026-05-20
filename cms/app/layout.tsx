819edf652b4ce1ecbd54ff490a23d114f414700c
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-20 23:18:42 +0700

    cms: tambah bagian 4 layout.tsx

--- cms/app/layout.tsx
+++ cms/app/layout.tsx
@@ -28,3 +28,13 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <AppProviders>
+          <AuthGuard>{children}</AuthGuard>
+        </AppProviders>
+      </body>
+    </html>
+  );
+}
