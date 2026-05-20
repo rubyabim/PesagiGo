662dadc276d1015ad5391e4f63788981fad65137
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-20 23:26:11 +0700

    cms: tambah bagian 1 query-client.ts

---
+++ cms/lib/core/query-client.ts
@@ -0,0 +1,8 @@
+﻿import { QueryClient } from '@tanstack/react-query';
+
+export function createQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 30,
+        retry: 1,
