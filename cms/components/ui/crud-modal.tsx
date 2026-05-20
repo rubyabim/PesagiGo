8567daf57657201592eca240db86a70f513e7657
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-20 23:26:10 +0700

    cms: tambah bagian 1 crud-modal.tsx

---
+++ cms/components/ui/crud-modal.tsx
@@ -0,0 +1,17 @@
+﻿'use client';
+
+import { ReactNode } from 'react';
+
+type Props = {
+  open: boolean;
+  title: string;
+  children: ReactNode;
+  onClose: () => void;
+};
+
+export default function CrudModal({ open, title, children, onClose }: Props) {
+  if (!open) {
+    return null;
+  }
+
+  return (
