7057f4ba3a2268149a20a6c11e366d67023645b1
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-23 23:48:25 +0700

    api: hapus admin service module

--- Api/src/admin-service/admin-service.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
-import { AdminModule } from '../admin/admin.module';
-
-@Module({
-  imports: [AuthModule, AdminModule],
-})
-export class AdminServiceModule {}
