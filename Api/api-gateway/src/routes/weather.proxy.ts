37e11151118788a3980f3abd68b0c8df1119e9f2
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-23 23:49:48 +0700

    api gateway: tambah proxy weather.proxy

---
+++ Api/api-gateway/src/routes/weather.proxy.ts
@@ -0,0 +1,17 @@
+import { All, Controller, Req, Res } from '@nestjs/common';
+import type { Request, Response } from 'express';
+import { ProxyBaseController } from './proxy.base';
+import { GatewayService } from './gateway.service';
+
+@Controller('api/weather')
+export class WeatherProxyController extends ProxyBaseController {
+  constructor(gatewayService: GatewayService) {
+    super(gatewayService);
+  }
+
+  @All()
+  @All('*')
+  handle(@Req() req: Request, @Res() res: Response) {
+    return this.forward(req, res);
+  }
+}
