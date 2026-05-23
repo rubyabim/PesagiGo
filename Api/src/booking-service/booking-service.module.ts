bca0134631acef2b22b32b0eba14a960c597ab3f
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-23 23:48:31 +0700

    api: hapus booking service module

--- Api/src/booking-service/booking-service.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
-import { BookingsModule } from '../bookings/bookings.module';
-
-@Module({
-  imports: [AuthModule, BookingsModule],
-})
-export class BookingServiceModule {}
