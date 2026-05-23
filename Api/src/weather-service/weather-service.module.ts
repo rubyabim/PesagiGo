6df27d6acc82723df51636682207e79fd8d61473
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-23 23:48:56 +0700

    api: hapus weather service module

--- Api/src/weather-service/weather-service.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WeatherModule } from '../weather/weather.module';
-
-@Module({
-  imports: [WeatherModule],
-})
-export class WeatherServiceModule {}
