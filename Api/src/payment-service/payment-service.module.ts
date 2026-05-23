604f492926acbca3e1412016ef6e5a02dfa641ba
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-23 23:48:51 +0700

    api: hapus payment service module

--- Api/src/payment-service/payment-service.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
-import { PaymentsModule } from '../payments/payments.module';
-
-@Module({
-  imports: [AuthModule, PaymentsModule],
-})
-export class PaymentServiceModule {}
