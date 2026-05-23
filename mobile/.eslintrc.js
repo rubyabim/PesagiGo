22123538e482de5d718c803c5d2ae3f1f2d9301e
Ruby.abim <abimfebriansyah2000@gmail.com>
2026-05-23 23:49:40 +0700

    mobile: perbarui .eslintrc

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['eslint:recommended'],
+  env: {
+    node: true,
+    jest: true,
+    'react-native/react-native': true,
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    requireConfigFile: false,
+  },
+  plugins: ['react', 'react-native'],
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+  },
 };
